06652227bb52a2b5ef0addaf399e652d3cda7fcb
lovish budhiraja <lovishbudhiraja@gmail.com>
2025-04-04 21:11:56 +0530

    db update again

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3333; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(process.env.PORT || 3333, () => {
+  console.log("JSON Server is running");
+});
